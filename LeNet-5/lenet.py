b22ece43558a48f25942617783e1d40e9e1b837c
Kanaad Limaye <kanaad.limaye@gmail.com>
2025-12-24 11:31:11 +0530

    Updated lenet code

--- LeNet-5/lenet.py
+++ LeNet-5/lenet.py
@@ -13,4 +13,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Device config.
-device = torch.
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+
+# Hyperparameters
+batch_size = 64
+lr = 0.001
+epochs = 50
+
+image_transform = transforms.Compose([
+    transforms.Pad(2),
+    transforms.ToTensor(),
+    transforms.Normalize(0,1)
+])
+
+train_dataset = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=image_transform)
+test_dataset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=image_transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
