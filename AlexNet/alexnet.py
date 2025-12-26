e7f3a39ebd0a6eb5fd78e50b0822ae187571afc1
Kanaad Limaye <kanaad.limaye@gmail.com>
2025-12-26 15:25:00 +0530

    Alexnet updated code

--- AlexNet/alexnet.py
+++ AlexNet/alexnet.py
@@ -1,2 +1,9 @@
 ## Implementation of AlexNet architecture in PyTorch from scratch.
-## Dataset used: Tiny 
+## Dataset used: MNIST
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
